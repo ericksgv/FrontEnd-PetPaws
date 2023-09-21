2e90abf3e91acdd86676bd4e9b8d431678b41472
Erick Garavito <sangaravi22@gmail.com>
2023-09-21 17:03:08 -0500

    Creación de las interfaces para todas las entidades

---
+++ proyecto/src/app/model/usuario.ts
@@ -0,0 +1,11 @@
+import { Mascota } from "./mascota";
+
+export interface Usuario {
+    id: number;
+    cedula: number;
+    nombre: string;
+    correo: string;
+    celular: number;
+    mascotas: Mascota[];
+  }
+  
