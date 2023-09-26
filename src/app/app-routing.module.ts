17cd514832cf01b00be93fbcc4833857838df111
Arduousbook <davids_barretom@javeriana.edu.co>
2023-09-25 23:32:06 -0500

    adicion de enrutamiento en landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,21 @@ import { MascotaTableComponent } from './mascota/mascota-table/mascota-table.com
 import { HeaderComponent } from './mascota/header/header.component';
 import { AgregarMascotaComponent } from './mascota/agregar-mascota/agregar-mascota.component';
 import { ModificarMascotaComponent } from './mascota/modificar-mascota/modificar-mascota.component';
+import { DropdownMenuComponent} from "./dropdown-menu/dropdown-menu.component";
+import { LandingPageComponent} from "./landing-page/landing-page.component";
 
 const routes: Routes = [
+
+  {
+    path: 'landingPage',
+    component: LandingPageComponent
+  },
+
+  {
+    path: 'dropdownMenu',
+    component: DropdownMenuComponent
+  },
+
   {
     path: 'mascotas/all',
     component: MascotaTableComponent,
@@ -15,9 +28,12 @@ const routes: Routes = [
     component: AgregarMascotaComponent,
   },
   {
-    path: 'mascotas/modificar/:id', 
+    path: 'mascotas/modificar/:id',
     component: ModificarMascotaComponent,
-  }
+  },
+
+
+
 ];
 
 @NgModule({
