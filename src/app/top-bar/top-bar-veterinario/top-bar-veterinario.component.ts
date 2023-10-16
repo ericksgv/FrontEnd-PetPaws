4d4cf97f3ff94cfa7618c7a9bb96013aa194db13
Arduousbook <davids_barretom@javeriana.edu.co>
2023-10-16 10:26:01 -0500

    Mejoras esteticas en top-bars

--- src/app/top-bar/top-bar-veterinario/top-bar-veterinario.component.ts
+++ src/app/top-bar/top-bar-veterinario/top-bar-veterinario.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-top-bar-veterinario',
   templateUrl: './top-bar-veterinario.component.html',
-  styleUrls: ['./top-bar-veterinario.component.css', '../../../styles.css']
+  styleUrls: ['./top-bar-veterinario.component.css', '../../../styles.css', '../top-bar.component.css']
 })
 export class TopBarVeterinarioComponent {
 
