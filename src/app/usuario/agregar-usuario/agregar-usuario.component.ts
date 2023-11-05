00aad90dd966754b8d4fd73eb5d22f7ffdd154dc
Daniel Gutierrez <danigutierritos2817@gmail.com>
2023-11-05 10:31:38 -0500

    quitar notificacion

--- src/app/usuario/agregar-usuario/agregar-usuario.component.ts
+++ src/app/usuario/agregar-usuario/agregar-usuario.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';  
 import { Router } from '@angular/router';
 import { UsuarioService } from '../Service/usuarioservice.service';  
-import { NotificationService } from '../../notification/Service/notification.service';
-import { NotificationComponent } from '../../notification/notification/notification.component';
+
 
 // Función de validación personalizada para el formato de correo electrónico
 export function EmailValidator(control: AbstractControl): { [key: string]: boolean } | null {
@@ -22,7 +21,7 @@ export function EmailValidator(control: AbstractControl): { [key: string]: boole
   templateUrl: './agregar-usuario.component.html',
   styleUrls: ['./agregar-usuario.component.css']
 })
-export class AgregarUsuarioComponent implements OnInit{
+export class AgregarUsuarioComponent {
   usuarioForm: FormGroup;  
   id: number | undefined;
   showError: boolean = false;
@@ -32,7 +31,6 @@ export class AgregarUsuarioComponent implements OnInit{
     private usuarioService: UsuarioService,  
     private router: Router,
     private formBuilder: FormBuilder, 
-    private notificationService: NotificationService,
   ) {
     this.usuarioForm = this.formBuilder.group({
       id: [null],
@@ -40,13 +38,6 @@ export class AgregarUsuarioComponent implements OnInit{
       nombre: ['', Validators.required],
       correo: ['', [Validators.required, EmailValidator]], // Usar la validación personalizada
       celular: ['', Validators.required]
-    });
-  }
-  ngOnInit() {
-    this.notificationService.getMessage().subscribe((message) => {
-      if (message) {
-        this.message = message; // Asignar el mensaje recibido a la propiedad message
-      }
     });
   }
   
@@ -57,11 +48,7 @@ export class AgregarUsuarioComponent implements OnInit{
       nuevoUsuario.estado = "activo";
       console.log(nuevoUsuario);
       this.usuarioService.agregarUsuario(nuevoUsuario).subscribe(() => {
-        console.log('Usuario agregado exitosamente');
-        this.notificationService.showMessage('Usuario agregado exitosamente');
-        setTimeout(() => {
           this.router.navigate(['usuario/all']);
-        }, 5000);
       });
     } else {
       this.mostrarError();
