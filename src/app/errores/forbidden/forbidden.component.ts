34ef8b2155565ccc559ec08aa1a66d469715d5a1
Arduousbook <davids_barretom@javeriana.edu.co>
2023-11-12 20:13:53 -0500

    Adicion de paginas de error

--- src/app/errores/forbidden/forbidden.component.ts
+++ src/app/errores/forbidden/forbidden.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-forbidden',
   templateUrl: './forbidden.component.html',
-  styleUrls: ['./forbidden.component.css']
+  styleUrls: ['./forbidden.component.css', '../unauthorized/unauthorized.component.css']
 })
 export class ForbiddenComponent {
 
